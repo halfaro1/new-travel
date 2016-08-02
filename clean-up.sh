d7f7831726d41dbf9ea0d265f0a027c0bd1e4f78
Hector Alfaro <hectorsector@github.com>
2016-08-02 11:25:26 -0400

    Update clean-up.sh

--- clean-up.sh
+++ clean-up.sh
@@ -1,3 +1,19 @@
-git rm mymap.topojson
-git commit -m "reset [ci skip]"
-git push --force --quiet "git@github.com:${GITHUB_REPO}.git" HEAD:master
+mv class-pins.topojson class-pins
+  if [ ! -f *.topojson ]; then
+    echo ":frown: there are no new topojson files!"
+    mv class-pins class-pins.topojson
+    git checkout master
+    git branch -d cleanup-and-merge
+    exit
+  else
+    echo ":tada: I found new files, combining and pushing now!"
+    geojson-merge *.topojson class-pins > class-pins.topojson
+    rm class-pins
+    mv class-pins.topojson class-pins
+    rm *.topojson
+    mv class-pins class-pins.topojson
+    git add -A
+    git commit -m "clean up class files"
+    git push -u origin cleanup-and-merge
+    echo "All done. Please go create the PR and merge if tests are passing!"
+  fi
