276cdadab1d7634f17e6e18eb3ade112a8ad452b
Hector Alfaro <hectorsector@github.com>
2016-08-04 11:56:49 -0400

    [skip ci] directly on master

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ echo "about to deploy..."
 git config user.name "Travis CI User"
 git config user.email "noone@noone.com"
 git config push.default simple
-git checkout -b cleanup-and-merge
+#git checkout -b cleanup-and-merge
+git checkout master
 mv class-pins.topojson class-pins
 echo "I found new files, combining and pushing now!"
 geojson-merge *.topojson class-pins > class-pins.topojson
@@ -12,9 +13,8 @@ mv class-pins.topojson class-pins
 rm *.topojson
 mv class-pins class-pins.topojson
 git add -A
-git commit -m "clean up class files"
-git checkout master
-git merge --no-ff --no-edit cleanup-and-merge
+git commit -m "[skip ci] clean up class files"
+#git merge --no-ff --no-edit cleanup-and-merge
 git remote set-url origin git@github.com:hectorsector/new-travel.git
 git push -u origin master
 echo "All done. Please go create the PR and merge if tests are passing!"
